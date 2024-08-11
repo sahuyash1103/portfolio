5a15787f700f0a75de8c03dcd725222edaf2a8c6
yash sahu <sahuyash1103@gmail.com>
2024-08-11 13:49:31 +0530

    vercel deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
